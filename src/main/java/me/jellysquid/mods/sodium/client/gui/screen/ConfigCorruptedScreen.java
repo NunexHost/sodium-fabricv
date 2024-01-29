3f2f744daf26487cc4df8ab58d04ed784fd495eb
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-29 12:35:38 -0300

    Update ConfigCorruptedScreen.java

--- src/main/java/me/jellysquid/mods/sodium/client/gui/screen/ConfigCorruptedScreen.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/screen/ConfigCorruptedScreen.java
@@ -3,8 +3,7 @@ package me.jellysquid.mods.sodium.client.gui.screen;
 import me.jellysquid.mods.sodium.client.SodiumClientMod;
 import me.jellysquid.mods.sodium.client.gui.console.Console;
 import me.jellysquid.mods.sodium.client.gui.console.message.MessageLevel;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
@@ -58,27 +57,27 @@ public class ConfigCorruptedScreen extends Screen {
             Console.instance().logMessage(MessageLevel.INFO, Text.translatable("sodium.console.config_file_was_reset"), 3.0);
 
             SodiumClientMod.restoreDefaultOptions();
-            MinecraftClient.getInstance().setScreen(this.nextScreen.apply(this.prevScreen));
+            Minecraft.getInstance().setScreen(this.nextScreen.apply(this.prevScreen));
         }).dimensions(this.width - SCREEN_PADDING - BUTTON_WIDTH, buttonY, BUTTON_WIDTH, BUTTON_HEIGHT).build());
 
         this.addDrawableChild(ButtonWidget.builder(Text.literal("Go back"), (btn) -> {
-            MinecraftClient.getInstance().setScreen(this.prevScreen);
+            Minecraft.getInstance().setScreen(this.prevScreen);
         }).dimensions(SCREEN_PADDING, buttonY, BUTTON_WIDTH, BUTTON_HEIGHT).build());
     }
 
     @Override
-    public void render(DrawContext drawContext, int mouseX, int mouseY, float delta) {
-        super.render(drawContext, mouseX, mouseY, delta);
+    public void render(int mouseX, int mouseY, float delta) {
+        super.render(mouseX, mouseY, delta);
 
-        drawContext.drawTextWithShadow(this.textRenderer, Text.literal("Sodium Renderer"), 32, 32, 0xffffff);
-        drawContext.drawTextWithShadow(this.textRenderer, Text.literal("Could not load the configuration file"), 32, 48, 0xff0000);
+        this.textRenderer.draw(Text.literal("Sodium Renderer"), 32, 32, 0xffffff);
+        this.textRenderer.draw(Text.literal("Could not load the configuration file"), 32, 48, 0xff0000);
 
         for (int i = 0; i < TEXT_BODY.size(); i++) {
             if (TEXT_BODY.get(i).getString().isEmpty()) {
                 continue;
             }
 
-            drawContext.drawTextWithShadow(this.textRenderer, TEXT_BODY.get(i), 32, 68 + (i * 12), 0xffffff);
+            this.textRenderer.draw(TEXT_BODY.get(i), 32, 68 + (i * 12), 0xffffff);
         }
     }
 }
